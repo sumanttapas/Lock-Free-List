0ff47db296281457d3efeb43774f86203327c199
sumanttapas <stapas@uncc.edu>
2016-05-21 12:58:08 -0400

    Linked List code with locks

---
+++ Linked.c
@@ -0,0 +1,116 @@
+/*
+ ============================================================================
+ Name        : Linkedlist.c
+ Author      : Sumant
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <ctype.h>
+
+
+typedef struct node
+{
+	int data;
+	struct node* next;
+}node_t;
+
+int c=0;
+
+void *thread1(void *);
+void *thread2(void *);
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void insert(int data,node_t * head)
+{
+
+	node_t * last = head;
+	pthread_mutex_lock(&mutex);
+	c++;
+
+	while(last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	if(last == head && c == 1)
+	{
+		last->data = data;
+	}
+
+	else if(c>1)
+	{
+		node_t * n = malloc(sizeof(struct node));
+		n->data = data;
+		//pthread_mutex_lock(&mutex);
+		last->next = n;
+		n->next = NULL;
+		//pthread_mutex_unlock(&mutex);
+
+	}
+	pthread_mutex_unlock(&mutex);
+
+}
+
+void printlist(node_t * head)
+{
+	do
+	{
+		printf("\t%d",head->data);
+		head = head->next;
+	}while(head != NULL);
+}
+
+node_t *init_list()
+{
+	node_t * head = malloc(sizeof(node_t));
+	head->next = NULL;
+	return head;
+}
+
+/*int main(void)
+{
+	node_t * head = init_list();
+	/*insert(1,head);
+	if(fork() == 0)
+	{
+		insert(13,head);
+		printf("\n");
+		printlist(head);
+		exit(0);
+	}
+	//wait(NULL);
+	insert(12,head);
+	insert(14,head);
+	printf("\n");
+
+	pthread_t t1,t2;
+	pthread_create (&t2, NULL, thread2, (void *)head);
+	pthread_create (&t1, NULL, thread1, (void *)head);
+
+	pthread_join (t1, NULL);
+	pthread_join (t2, NULL);
+	printlist(head);
+	return 0;
+}*/
+
+void * thread1(void * args)
+{
+	node_t * head = args;
+	insert(12,head);
+	insert(14,head);
+}
+
+void * thread2(void * args)
+{
+	node_t * head = args;
+	insert(22,head);
+	insert(24,head);
+}
+
