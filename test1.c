6500adfb169a2c712380ef19099eb508b3b79161
rsingh16 <riteshkumarsingh16@gmail.com>
2016-06-15 01:49:29 -0400

    Add files via upload

--- test1.c
+++ test1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-static inline unsigned long
-cas(volatile unsigned long* ptr, unsigned long old, unsigned long _new)
+static inline int
+cas(volatile int* ptr, int old, int _new)
 {
-    unsigned long prev=0;
-    __asm__ __volatile("lock cmpxchg8b %0;"
-                 : "=m"(prev)
-                 : "m"(*ptr),"a"(old),"c"(_new)
+    int prev=0;
+    __asm__ __volatile("lock cmpxchg8b %[ptr]\n\t"
+                : [prev] "=q" (prev), [ptr] "+m" (*ptr),"+d" (old)
+                 : "c" (_new)
                  );
     return prev;
 }
@@ -15,11 +15,10 @@ cas(volatile unsigned long* ptr, unsigned long old, unsigned long _new)
 int main()
 {
 	
-	unsigned long *a;
-	unsigned long b=5,c;
+	int *a;
+	int b=3,c;
 	a=&b;
         c=cas(a,b,6);
-	printf("%lu\n",c);
+	printf("%d\n",c);
 	return 0;
 }
-	
